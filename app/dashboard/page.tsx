111af01034ac133e1d3ccb64eaeb24bb18de37bb
Daniel Bowden <dj.bowden7@gmail.com>
2024-09-05 09:30:27 -0400

    put back user service

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,8 +5,7 @@ import { columns } from "./video-upload-columns";
 import { VideoUpload } from "./video-upload-form";
 
 export default async function DashboardPage() {
-    // const user = await UserService.getUser();
-    const user = { username: 'test'};
+    const user = await UserService.getUser();
   return (
     <main className="flex-1 flex flex-col gap-6 px-4">
     <h2 className="font-medium text-xl mb-4">Set up your account!</h2>
